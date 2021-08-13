213cdfd86774d5ef22ef6b8a2b4e0642cadce98d
Noah Mercado <noahmercado@google.com>
2021-08-13 00:54:03 -0400

    adjust variable naming

--- analyze_project_move.sh
+++ analyze_project_move.sh
@@ -10,15 +10,13 @@
 # Need resourcemanager.projects.getIamPolicy on the project being moved
 # Need orgpolicy.policy.get on project being moved
 # Need compute.organizations.setSecurityPolicy on project being moved folder or org
-# export PROJECT_ID=<PROJECT_BEING_MOVED_ID>
-# export DESTINATION_ID=folders/<DESTINATION_ID>
+# export DESTINATION_ORG_ID=<DESTINATION_ID>
 
-
-DESTINATION_ID=""
+DESTINATION_ORG_ID=""
 
 for PROJECT_ID in $(gcloud projects list --format="value(project_number)")
 do
     gcloud asset analyze-move --project="${PROJECT_ID}" \
-        --destination-organization="${DESTINATION_ID}" \
-        > migrate_${PROJECT_ID}_to_${DESTINATION_ID}_analysis.txt
+        --destination-organization="${DESTINATION_ORG_ID}" \
+        > migrate_${PROJECT_ID}_to_${DESTINATION_ORG_ID}_analysis.txt
 done
